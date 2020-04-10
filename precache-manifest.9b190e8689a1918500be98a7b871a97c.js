f461fcf382bf220cdc55c5c962ce3a7ee266d452
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-10 09:18:10 -0400

    Updates

---
+++ precache-manifest.9b190e8689a1918500be98a7b871a97c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a06801077c908d2d08d",
+    "url": "/ReactJS/static/css/main.531d5c1a.chunk.css"
+  },
+  {
+    "revision": "1a06801077c908d2d08d",
+    "url": "/ReactJS/static/js/main.1a068010.chunk.js"
+  },
+  {
+    "revision": "66332ccd94fbb15c1d56",
+    "url": "/ReactJS/static/js/runtime~main.66332ccd.js"
+  },
+  {
+    "revision": "74ea7b36c664d2e6f099",
+    "url": "/ReactJS/static/css/2.9bc10368.chunk.css"
+  },
+  {
+    "revision": "74ea7b36c664d2e6f099",
+    "url": "/ReactJS/static/js/2.74ea7b36.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/ReactJS/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/ReactJS/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/ReactJS/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "b6f6b911fa564b64af7d0bbad8308eec",
+    "url": "/ReactJS/static/media/img404.b6f6b911.png"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/ReactJS/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/ReactJS/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "f7ffb44b217ca03155387c4886e83fd4",
+    "url": "/ReactJS/index.html"
+  }
+];
